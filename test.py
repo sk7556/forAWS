41410ed42b2e1573142d4e5cccd716a33b7fcf68
Jungsik <sk7556@naver.com>
2024-01-05 18:00:02 +0900

    🚀 크롤링 가능한 py, pip 환경 인스톨

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-a = [1,2,3]
+import requests
+from bs4 import BeautifulSoup
 
-print(a)
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+bookservices = soup.select('.title_text')
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
